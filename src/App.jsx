510bda3282a21990a38517eb25d9750f729258a0
Arthur Rocha <arthsx7@gmail.com>
2023-11-08 19:10:22 -0300

    drum-machine

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
-import { useState, useEffect } from "react";
+import data from "../db.json";
 
 import { DrumPad } from "./components/DrumPad.jsx";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/songs", {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setData(data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
   return <DrumPad drums={data} />;
 }
 
